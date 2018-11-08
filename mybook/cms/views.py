7e392ec3c56a00e71827d7dc787e626d6e5fb366
ydozen <ydozen@DozennoMacBook-ea.local>
2018-11-08 10:07:41 +0900

    初回

---
+++ mybook/cms/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from cms.models import Book
+
+def book_list(request):
+    # 書籍の一覧
+    # return HttpResponse('書籍の一覧')
+    books = Book.objects.all().order_by('id')
+    return render(request,
+                    'cms/book_list.html',   # 使用するテンプレート
+                    {'books': books})       # テンプレートに渡すデータ
+
+def book_edit(request, book_id=None):
+    # 書籍の編集
+    return HttpResponse('書籍の編集')
+
+def book_del(request, book_id):
+    #　書籍の削除
+    return HttpResponse('書籍の削除')
