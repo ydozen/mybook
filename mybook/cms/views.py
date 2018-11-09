599489999646003ca2c7da5bf0c9657935b0ab01
ydozen <ydozen@DozennoMacBook-ea.local>
2018-11-09 14:37:15 +0900

    一覧のテンプレを作る

--- mybook/cms/views.py
+++ mybook/cms/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from cms.models import Book
 
 def book_list(request):
     # 書籍の一覧
-    return HttpResponse('書籍の一覧')
+    #return HttpResponse('書籍の一覧')
+    books = Book.objects.all().order_by('id')
+    return render(request,
+                'cms/book_list.html',
+                {'books': books})
 
 def book_edit(request, book_id=None):
     # 書籍の編集
